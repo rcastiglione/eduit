16b14245497de84c8786e9ab33298a9327ef3e3f
Ricardo Castiglione <ricardo.castiglione@gmail.com>
2019-05-07 21:47:33 -0300

    first commit

---
+++ src/main/java/com/eduit/example/App.java
@@ -0,0 +1,13 @@
+package com.eduit.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
